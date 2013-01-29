3a84e0ffa156ccac8d83838e6c455687deb5dee4
Romain Bellina <Bilou@bilou-mac-lpdev.etudiants.loc>
2013-01-29 11:05:29 +0100

    update

--- app/cache/dev/appdevUrlMatcher.php
+++ app/cache/dev/appdevUrlMatcher.php
@@ -162,6 +162,11 @@ class appdevUrlMatcher extends Symfony\Bundle\FrameworkBundle\Routing\Redirectab
 
         }
 
+        // sensio_training_default_index
+        if (0 === strpos($pathinfo, '/hello') && preg_match('#^/hello/(?P<name>[^/]+)$#s', $pathinfo, $matches)) {
+            return array_merge($this->mergeDefaults($matches, array (  '_controller' => 'Sensio\\Bundle\\TrainingBundle\\Controller\\DefaultController::indexAction',)), array('_route' => 'sensio_training_default_index'));
+        }
+
         throw 0 < count($allow) ? new MethodNotAllowedException(array_unique($allow)) : new ResourceNotFoundException();
     }
 }
---
+++ app/cache/dev/twig/30/49/6ce22d1e8767a86d37d96be7bd79.php
@@ -0,0 +1,51 @@
+<?php
+
+/* SensioTrainingBundle:Default:index.html.twig */
+class __TwigTemplate_30496ce22d1e8767a86d37d96be7bd79 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_body($context, array $blocks = array())
+    {
+        // line 3
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "SensioTrainingBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 3,  28 => 2,);
+    }
+}
