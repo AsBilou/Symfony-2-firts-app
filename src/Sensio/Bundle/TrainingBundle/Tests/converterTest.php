19a2682ee376a6cf3ffa77dac4ea4361900ffd9a
Romain Bellina <contact@romainbellina.fr>
2013-01-31 09:56:56 +0100

    update

---
+++ src/Sensio/Bundle/TrainingBundle/Tests/converterTest.php
@@ -0,0 +1,11 @@
+<?php
+    namespace Sensio\Bundle\TrainingBundle\Test;
+
+    use Sensio\Bundle\TrainingBundle\Converter\CelsiusConverter;
+
+    class ConverterTest extends \PHPUnit_Framework_TestCase{
+        public function testFahrenheit(){
+            //
+        }
+    }
+?>
